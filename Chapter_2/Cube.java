52c02b921917c53e80c2eb7fb28fcdf7683226ee
VektorHL <vektorhalflife.vektorhl@gmail.com>
2022-07-07 17:17:50 +0300

    залил, что было

---
+++ Chapter_2/Cube.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Cube {
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+
+        //кусок кода с JavaRush с потоками, принимает массив интов, возвращает Лист четных чисел;
+        int[] array = {1, 2, 3};
+        System.out.println(asd(array));
+    }
+
+    public static List<Integer> asd(int[] array) {
+        return Arrays.stream(array)
+                .filter(num -> num % 2 == 0)
+                .boxed()
+                .collect(Collectors.toList());
+
+    }
+}
